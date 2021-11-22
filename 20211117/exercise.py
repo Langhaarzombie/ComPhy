ba5a722e32905907b393eb9839bca3e6633621d8
langhaarzombie <mixmax199989@gmail.com>
2021-11-22 17:05:06 +0100

    Corrected boost, all calculations in SI now

--- 20211117/exercise.py
+++ 20211117/exercise.py
@@ -3,10 +3,18 @@ import numpy.linalg as linalg
 import scipy.constants as const
 import matplotlib.pyplot as plt
 
-me = 5.972e24 # mass of the earth
-r0 = 42157 # the initial distance from earth
+"""
+Comparison of explicit and symplectic euler:
+Explicit starts to deviate from a usable solution at around dt = 40
+whereas the symplectic method is still ok at dt = 5000.
+This fact of course saves valuable computation time.
+"""
 
-def euler_expl(dt, stop=15):
+me = 5.972e24 # mass of the earth
+r0 = 42157000 # the initial distance from earth
+boost = 0.01 # amount of boost to be fired (times satellite mass)
+
+def euler_expl(dt, stop=1000000):
     """
     Args:
         dt   ... timestep for expl. Euler method
@@ -38,11 +46,11 @@ def euler_expl(dt, stop=15):
 
         if t/T > 2 and t/T < 3:
             v_abs = linalg.norm(vn)
-            vn = vn + (vn/v_abs) * dt * 5000
+            vn = vn + (vn/v_abs) * dt * boost
 
     return ts, rs
 
-def euler_sympl(dt, stop=15):
+def euler_sympl(dt, stop=1000000):
     """
     Args:
         dt   ... timestep for expl. Euler method
@@ -58,38 +66,40 @@ def euler_sympl(dt, stop=15):
     # initial values for the satellite
     rs = np.array([[r0, 0]])
 
-    # Calculated values so that the satellite stays in orbit
-    # for any kind of orbit
+    # initial values so that the satellite stays in orbit
     T = 2 * const.pi * np.sqrt(r0**3 / (const.G * me))
-    # vn = [0, 2 * const.pi * r0 / T]
-    # for perfectly circular orbit
     vn = np.array([0, np.sqrt((const.G * me) / r0)])
 
     for t in ts:
-        r_abs = linalg.norm(rs[-1])
         rn = rs[-1] + dt * vn
+
+        r_abs = linalg.norm(rn)
         vn = vn + dt * (- const.G * me / r_abs**3 ) * rn
 
         rs = np.concatenate((rs, [rn]), axis=0)
 
         if t/T > 2 and t/T < 3:
             v_abs = linalg.norm(vn)
-            vn = vn + (vn/v_abs) * dt * 5000
+            vn = vn + (vn/v_abs) * dt * boost
 
     return ts, rs
 
 if __name__ == "__main__":
-    _, ee_rs = euler_expl(0.01)
-    _, es_rs = euler_sympl(0.01)
+    _, ee_rs = euler_expl(50)
+    _, es_rs = euler_sympl(1000)
 
-    _, ax = plt.subplots(2)
+    # _, ax = plt.subplots(2)
 
-    ax[0].plot(ee_rs[:,0], ee_rs[:,1])
-    ax[0].plot([0], [0], "o")
-    ax[0].plot(ee_rs[-1,0], ee_rs[-1,1], "o")
+    # ax[0].plot(ee_rs[:,0], ee_rs[:,1])
+    # ax[0].plot([0], [0], "o")
+    # ax[0].plot(ee_rs[-1,0], ee_rs[-1,1], "o")
 
-    ax[1].plot(es_rs[:,0], es_rs[:,1])
-    ax[1].plot([0], [0], "o")
-    ax[1].plot(es_rs[-1,0], es_rs[-1,1], "o")
+    # ax[1].plot(es_rs[:,0], es_rs[:,1])
+    # ax[1].plot([0], [0], "o")
+    # ax[1].plot(es_rs[-1,0], es_rs[-1,1], "o")
+
+    plt.plot(es_rs[:,0], es_rs[:,1])
+    plt.plot([0], [0], "o")
+    plt.plot(es_rs[-1,0], es_rs[-1,1], "o")
 
     plt.show()
