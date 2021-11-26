267ff44cb8916464de110a4cbc96733b94ef4ef9
langhaarzombie <mixmax199989@gmail.com>
2021-11-26 15:28:26 +0100

    Added method with sparse matrix

--- 20211126/exercise.py
+++ 20211126/exercise.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.sparse as sp
+from scipy.sparse.linalg import spsolve
 import scipy.linalg as linalg
 import matplotlib.pyplot as plt
 
@@ -20,20 +22,42 @@ def with_dense_matrix(N):
     # We could solve tha by simply appending to the results array, but it would
     # make the solution so much uglier. That is why I omitted it.
 
+def with_sparse_matrix(N):
+    D = N**2
+    y0 = 1
+
+    # we need to define the format of the sparse matrix as reshaping etc. is not available
+    # with the default format
+    A = (-4) * sp.eye(D, format='csr') + sp.eye(D, k=-1) + sp.eye(D, k=1) + sp.eye(D, k=N) + sp.eye(D, k=-N)
+    for n in np.arange(N, D, step=N):
+        A[n, n-1] = 0
+        A[n-1, n] = 0
+
+    b = np.zeros(D)
+    b[:N] = -y0
+
+    np.savetxt('sparse_matrix_solved.dat', spsolve(A, b))
 
 if __name__ == "__main__":
     N = 50 #size of square grid
 
     with_dense_matrix(N)
+    with_sparse_matrix(N)
 
     DS = np.array(np.loadtxt('dense_matrix_solved.dat'))
+    SS = np.array(np.loadtxt('sparse_matrix_solved.dat'))
 
+    _, ax = plt.subplots(1, 2)
     # makes a heat map from the values in the results matrix
     # origin lower sets the start to be from the lower left (else it would
     # start at the top)
     # extent tells the axis to go from 0 to 1
     # cmap chooses a nice looking color map for the heat map
-    plt.imshow(np.reshape(DS, (N, N)), extent=[0,1,0,1], cmap='jet', origin='lower')
-    plt.colorbar()
+    im1 = ax[0].imshow(np.reshape(DS, (N, N)), extent=[0,1,0,1], cmap='jet', origin='lower')
+    im2 = ax[1].imshow(np.reshape(SS, (N, N)), extent=[0,1,0,1], cmap='jet', origin='lower')
+    ax[0].set_title("Dense matrix")
+    ax[1].set_title("Sparse matrix")
+    plt.colorbar(im1, ax=ax[0], shrink=0.5)
+    plt.colorbar(im2, ax=ax[1], shrink=0.5)
     plt.show()
 
